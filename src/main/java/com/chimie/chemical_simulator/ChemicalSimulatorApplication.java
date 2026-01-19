9d3cd067784b4675f3b9ee2adecf5577c880ff6e
S-v-3-n <sven.mrs.n@gmail.com>
2026-01-19 16:10:45 +0100

    init

---
+++ src/main/java/com/chimie/chemical_simulator/ChemicalSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.chimie.chemical_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemicalSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChemicalSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chimie/chemical_simulator/ChemicalSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chimie.chemical_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChemicalSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
